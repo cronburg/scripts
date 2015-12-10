220b2c35896dd775e1bf9ba67c7a6918912f1778
Karl Cronburg <karl.cronburg@gmail.com>
2015-12-09 23:58:30 -0500

    fun coding dir

---
+++ fun/true-false.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define TRUE  ('/'/'/')
+#define FALSE ('-'-'-')
+
+int main(int argc, char* argv[]) {
+  printf("TRUE=%d\n", (int)TRUE);
+  printf("FALSE=%d\n", (int)FALSE);
+}
+
